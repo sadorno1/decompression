46130061c8ea3d46713fccce1585b5615f7e5ec4
karen middleton <karenmiddleton@ku.edu>
2025-07-16 16:22:41 -0500

    Added predictions_plot.m

---
+++ predictions_plot.m
@@ -0,0 +1,15 @@
+h_fig = figure(1); clf(h_fig); set(h_fig,'WindowStyle','docked');
+plot(y_train)
+hold on;
+plot(train_predictions)
+grid on;
+legend('y train', 'train predictions');
+
+
+
+h_fig = figure(2); clf(h_fig); set(h_fig,'WindowStyle','docked');
+plot(y_test)
+hold on;
+plot(predictions)
+grid on;
+legend('y test', 'predictions');
